db7ea46e2e067280d504d0dd4ffb37817087a003
Feriyal Susanto <feriyalsusanto@gmail.com>
2026-03-06 10:38:50 +0700

    jekyll integration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/about-feriyalsusanto",
+  assetPrefix: "/about-feriyalsusanto",
 };
 
 export default nextConfig;
